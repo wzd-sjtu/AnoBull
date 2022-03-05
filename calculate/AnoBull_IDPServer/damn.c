665bb63edae10f71421730f67c71f511a7c655c9
wzd <wzdwxjwxw@sjtu.edu.cn>
2022-03-05 16:43:07 +0800

    delete calculate

--- calculate/AnoBull_IDPServer/damn.c
+++
@@ -1,9 +0,0 @@
-#include "/usr/local/include/pbc/pbc.h"
-#include "/usr/local/include/pbc/pbc_test.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include <string.h>
-#include <openssl/sha.h>
-
-#include "all_def.h"
-
